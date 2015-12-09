b8042f22dc12d8369dae5bad33ea814c16768bb5
Will Gallia <wgallia@gmail.com>
2015-12-09 23:40:51 +0000

    day 7

---
+++ day7.py
@@ -0,0 +1,50 @@
+import re
+
+regex = re.compile(r'([a-z0-9]+)? ?(AND|OR|LSHIFT|RSHIFT|NOT)? ?([a-z0-9]+)? -> ([a-z]+)')
+
+def parse(l):
+    r = re.search(regex, l).groups()
+    return (r[3], r[:3])
+
+table = {}
+def value(a):
+    global table, data
+
+    if re.search('[a-z]', a):
+        try:
+            return table[a]
+        except KeyError:
+            val = evaluate(data[a])
+            table[a] = val
+            return val
+    else:
+        return int(a)
+    
+def evaluate(instruction):
+    global data
+    
+    a, operation, b = instruction
+    
+    if operation == None:
+        return value(a)
+    elif operation == 'NOT':
+        return ~value(b) & (2*16-1)
+    elif operation == 'AND':
+        return value(a) & value(b)
+    elif operation == 'OR':
+        return value(a) | value(b)
+    elif operation == 'LSHIFT':
+        return value(a) << value(b)
+    elif operation == 'RSHIFT':
+        return value(a) >> value(b)
+
+
+with open('data/7.data') as f:
+    lines = f.readlines()
+
+data = dict(map(parse, lines))
+
+#print(evaluate(data['a']))
+
+data['b'] = ('46065', None, None)
+print(evaluate(data['a']))
